c9275122841527496ebef5231d9db2ac8b770a63
DevJoaoSales <pmagal539@gmail.com>
2023-08-22 16:17:24 -0300

    correcao

--- study/script.js
+++ study/script.js
@@ -36,20 +36,20 @@ function playText() {
   const repetitionsSelect = document.getElementById('repetitions-select');
   const selectedRepetitions = parseInt(repetitionsSelect.value);
 
-  document.getElementById("txtSelect").innerText = selectedText;
+  //document.getElementById("txtSelect").innerText = selectedText;
   
-  /*let test;
+  let test;
   speechSynthesis.getVoices().forEach( (iten)=>{
-    test += iten.voiceURI+" | ";
+    test += iten.lang+" | ";
   } );
 
   document.getElementById("txtSelect").innerText = test;
-*/
+
 
   // Para cancelar a reprodução
   speechSynthesis.cancel();
 
-  const enUSVoice = speechSynthesis.getVoices().find(voice => voice.lang === "en-US" || voice.lang === "inglês Estados Unidos" );
+  const enUSVoice = speechSynthesis.getVoices().find(voice => voice.lang === "en-US" );
   
   if (enUSVoice) {
     const audio = new SpeechSynthesisUtterance(selectedText);
